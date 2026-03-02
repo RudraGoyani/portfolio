374a2b5caf6b37b0a7392936114ebdaff6bc6554
RudraGoyani <rudraraj.006@gmail.com>
2026-03-01 22:25:50 -0600

    update code

--- first.js
+++ first.js
@@ -1,17 +1,19 @@
-// async function datafetch() {
-//     const responce = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
-//     const data=await responce.json();
-//     console.log(data.name);
-// }
-// datafetch();
-// var buton= document.getElementById('Fetch data');
-// buton.addEventListener('click',datafetch);
+
 async function loadPokemon() {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
-  const data = await response.json();
-    const dataElement = document.getElementById("data");
-    dataElement.textContent = data.name;
-  console.log(data.name);
-}
+try {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+Math.floor(Math.random() * 100));
+  if (!response.ok) {
+    console.error("Network response was not ok:", response.statusText);
+    return;
+  }else {
+    const data = await response.json();
+      const dataElement = document.getElementById("data");
+      dataElement.textContent = data.name;
+    console.log(data.name);
+  }}
+catch (error) {
+  console.error("Error fetching data:", error);
+  return;
+}}
 
 loadPokemon();
